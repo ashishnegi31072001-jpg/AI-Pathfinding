4a39ffbccd4a859a790fe0e439da547df473450c
Priyanshu Nautiyal <priyanshu96p@gmail.com>
2026-03-31 10:17:54 +0530

    inputfile

--- input.c
+++ input.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include "grid.h"
+#include "input.h"
 
-void placeObstacle(int x, int y) {
-    if(x >= 0 && x < MAX && y >= 0 && y < MAX) {
-        grid[x][y].isObstacle = 1;
-    }
+void getGridSize(int *rows, int *cols) {
+    printf("Enter rows and cols: ");
+    scanf("%d %d", rows, cols);
+}
+
+void getStartGoal(int *sx, int *sy, int *gx, int *gy) {
+    printf("Enter Start position (row col): ");
+    scanf("%d %d", sx, sy);
+
+    printf("Enter Goal position (row col): ");
+    scanf("%d %d", gx, gy);
+}
+
+int getObstacles() {
+    int obstacles;
+    printf("Enter number of obstacles: ");
+    scanf("%d", &obstacles);
+    return obstacles;
+}
+
+int getWeightedChoice() {
+    int choice;
+    printf("\nEnter 0 for Unweighted Graph\n");
+    printf("Enter 1 for Weighted Graph\n");
+    printf("Your choice: ");
+    scanf("%d", &choice);
+    return choice;
 }
--- input.h
+++ input.h
@@ -1,6 +1,11 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-void placeObstacle(int x, int y);
+#define MAX 50
+
+void getGridSize(int *rows, int *cols);
+void getStartGoal(int *sx, int *sy, int *gx, int *gy);
+int getObstacles();
+int getWeightedChoice();
 
 #endif
